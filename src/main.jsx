34b36d54397398b21d2ab5e17214777cd98370e6
Julie Zhang <juliezhg@stanford.edu>
2025-12-02 14:25:19 -0800

    Remove missing CSS import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // optional if you have a CSS file
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
